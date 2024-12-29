e23abf625d7a56d5587fb7933b19eeb3b8dedba8
lf-iacob <iacob.laura.f@gmail.com>
2024-12-29 19:03:18 +0100

    Update plasma_debye.py

--- plasma_debye.py
+++ plasma_debye.py
@@ -13,7 +13,7 @@ def pot_Q(Q, x):
   return Q/(4*np.pi*eps_0*x)
 def pot_plasma(Q, T, n_0, x):
   l_d=np.sqrt((eps_0*k_B*T)/(n_0*elet*elet))
-  return (Q*np.exp(-x/l_d))/(4*np.pi*eps_0*x)
+  return (Q*np.exp(-x/l_d))/(4*np.pi*eps_0*x), l_d
 
 #dati simulati
 r=np.arange(8*pow(10, -6), 300*pow(10, -6), 4*pow(10, -6))
@@ -21,27 +21,31 @@ Q=4*pow(10, -19)
 n_0=100*10**12
 T=100
 carica=pot_Q(Q, r)
-plasma=pot_plasma(Q, T, n_0, r)
+plasma,l=pot_plasma(Q, T, n_0, r)
 
 #rappresentazione grafica
 plt.figure()
 plt.plot(r*10**6, carica*10**3, color='mediumseagreen', label='Carica puntiforme')
 plt.plot(r*10**6, plasma*10**3, color='mediumvioletred', label='Plasma perturbato')
+plt.scatter(np.full(12,l*10**6), np.linspace(0, 0.43, 12), marker='|', color='orangered', label='λD')
 plt.title('Potenziale elettrico φ')
 plt.xlabel('r (µm)')
 plt.ylabel('φ(r) (mV)')
 plt.legend()
 plt.grid(linestyle=':')
 plt.show()
+plt.legend()
+plt.show()
 
 plt.figure()
 plt.plot(r*10**6, carica*10**3, color='mediumseagreen', label='Carica puntiforme')
 plt.plot(r*10**6, plasma*10**3, color='mediumvioletred', label='Plasma perturbato')
+plt.scatter(np.full(12,l*10**6), pow(10, np.linspace(-3.8, -0.5, 12)), marker='|', color='orangered', label='λD')
 plt.title('Potenziale elettrico φ')
 plt.xlabel('r (µm)')
-plt.xscale('log')
 plt.ylabel('φ(r) (mV)')
+plt.legend()
+plt.xscale('log')
 plt.yscale('log')
 plt.grid(linestyle=':')
-plt.legend()
 plt.show()
